61f3ac662d608d461bee8502c7d9a06dc798d7be
Vinay Daripelly <152810364+Vinay-Daripelly@users.noreply.github.com>
2025-11-14 23:38:42 +0530

    Update output message in Main class

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World This is Vinay!!!!!!!!!!!!!!");
+        System.out.println("Hello World This is Vinay!!!!!!!!!!!!!!\n i am here to save world");
     }
 }
+
 
